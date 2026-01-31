3e52a3e3e6f72981044f7a6810e4151da20cc6e0
Gohil Gautam <gohil.g.m9764@gmail.com>
2026-01-31 10:29:50 +0530

    day-3 assignment

---
+++ day-3/assignment.js
@@ -0,0 +1,19 @@
+
+
+console.log("assignment Operations in JavaScript");
+
+let x = 20;
+let y = 10; 
+console.log("Initial Values of x and y:", x , y);
+
+x += y; // x = x + y
+console.log("After x += y, x:", x); 
+x -= y; // x = x - y
+console.log("After x -= y, x:", x); 
+x *= y; // x = x * y
+console.log("After x *= y, x:", x);
+x /= y; // x = x / y
+console.log("After x /= y, x:", x);
+x %= y;// x = x % y
+console.log("After x %= y, x:", x);
+console.log("Modulus: " + (x % y));
