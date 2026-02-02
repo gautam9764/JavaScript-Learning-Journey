71dbc88f6899f8e27c7a5e2e080a8d73f84b3fe7
Gohil Gautam <gohil.g.m9764@gmail.com>
2026-02-02 21:01:47 +0530

    day-3 app m1

--- day-3/app.js
+++ day-3/app.js
@@ -107,62 +107,15 @@
 }
 
 {
-  // if Statement
-  console.log("if Statement");
-  let age = 33;
-
-  if (age <= 33) {
-    console.log("Fail");
-  }
-}
-
-{
-  // if-else Statement
-  console.log("if-else Statement");
-  let age = 34;
-
-  if (age <= 33) {
-    console.log("Fail");
-  } else {
-    console.log("Pass");
-  }
-}
-
-{
-  // else-if Statement
-  console.log("else-if Statement");
-  let age =  18
-  
-  if(age < 18){
-    console.log("junior");
-  }else if (age > 18){
-    console.log("senior");
-  }else{
-    console.log("middle");
-  }
-}
+  // Ternary Operators
+  let a = 5;
+  let b = 2;
+  console.log("a =", a, "& b =", b);
 
 
+  let max = (a > b) ? a : b;
+  console.log("The maximum value between a and b is:", max);
 
-{
-  let score = 85; // Change this value to test other scores
-let grade;
-
-if (score >= 80 && score <= 100) {
-  grade = "A";
-} else if (score >= 70 && score <= 79) {
-  grade = "B";
-} else if (score >= 60 && score <= 69) {
-  grade = "C";
-} else if (score >= 50 && score <= 59) {
-  grade = "D";
-} else if (score >= 0 && score <= 49) {
-  grade = "F";
-} else {
-  grade = "Invalid score";
-}
-
-console.log("Score:", score);
-console.log("Grade:", grade);
-
+  a < b ? console.log("a is greater than b") : console.log("a is not greater than b");
+  a > b ? console.log("a is greater than b") : console.log("a is not greater than b");
 }
