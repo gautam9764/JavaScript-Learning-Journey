79a78ec8776f4f7b82f28fd2bec045d91924756e
Gohil Gautam <gohil.g.m9764@gmail.com>
2026-02-09 10:23:16 +0530

    day-6 function_expression

---
+++ day-6/function_expression.js
@@ -0,0 +1,11 @@
+ // function expression
+
+ console.log("function expression");
+
+   let sum2 = function (a, b) {
+    return a + b;
+  };
+
+  let a = sum(2, 3);   // 5
+  let b = sum2(2, 3); // 5
+  console.log(a, b);  // output: 5 5
