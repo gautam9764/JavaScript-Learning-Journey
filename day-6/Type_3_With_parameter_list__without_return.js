bd24bb46b7a76e2d580e923ef4acee1147399174
Gohil Gautam <gohil.g.m9764@gmail.com>
2026-02-11 08:13:22 +0530

    day-6 Type_3_With_parameter_list__without_return

---
+++ day-6/Type_3_With_parameter_list__without_return.js
@@ -0,0 +1,8 @@
+//Type 3. With parameter list & without return
+console.log("Type 3. With parameter list & without return");
+  function gamma(str) {
+    console.log("hello bro", str);
+    // output: hello bro manas
+  }
+
+  gamma("manas");
