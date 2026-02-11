761f9362bcfa782eaac2e48e5486d264b1aa1b40
Gohil Gautam <gohil.g.m9764@gmail.com>
2026-02-11 08:12:58 +0530

    day-6 Type_2_Without_parameter_list__with_return

---
+++ day-6/Type_2_Without_parameter_list__with_return.js
@@ -0,0 +1,9 @@
+ //Type2. Without parameter list & with return
+
+ console.log("Tyoe2. Without parameter list & with return");
+  function beta() {
+    return "Mkl"; // value return કરે છે
+  }
+
+  let a = beta(); // a = "Mkl"
+  console.log(a); // output: Mkl
