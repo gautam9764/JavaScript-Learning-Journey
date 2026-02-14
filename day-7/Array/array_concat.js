74b0c882b5b6e623f5f65d756ac8169e28d2206e
Gohil Gautam <gohil.g.m9764@gmail.com>
2026-02-14 11:57:06 +0530

    day-7 array_concat

---
+++ day-7/Array/array_concat.js
@@ -0,0 +1,10 @@
+
+
+console.log("array concat")
+
+let arr = [2, 5, "mkl", 5, 8, 10, "alpha", "beta"];
+let arr2 = [5, 6];
+let arr3 = ["mkl", "falna", "dhikna"];
+let newArr = arr.concat(arr2, arr3);
+console.log(arr);
+console.log(newArr);
