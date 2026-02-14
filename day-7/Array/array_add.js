f0beb1af1d4b17c407e85976dcba16dd8d671659
Gohil Gautam <gohil.g.m9764@gmail.com>
2026-02-14 11:56:40 +0530

    day-7 array_add

---
+++ day-7/Array/array_add.js
@@ -0,0 +1,10 @@
+    
+    console.log("array elem add");
+
+    let arr = [1, 3, 5, 8];
+    console.log(arr[0]);
+    console.log(arr[2]);
+    arr[2] = 99;
+    console.log(arr);
+    arr[0] = 100;
+    console.log(arr);
