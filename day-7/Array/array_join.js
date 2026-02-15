552dad2731d5fa09890dededecfc359cebf8f1f3
Gohil Gautam <gohil.g.m9764@gmail.com>
2026-02-15 10:31:25 +0530

    day-7 array_join

---
+++ day-7/Array/array_join.js
@@ -0,0 +1,10 @@
+
+
+console.log("array join");
+
+let fruits = ["mango", "litchi", "guava", "mango", "papita"];
+
+let test = fruits.join(" And ");
+console.log(fruits);
+console.log(test);
+console.log(typeof test);
