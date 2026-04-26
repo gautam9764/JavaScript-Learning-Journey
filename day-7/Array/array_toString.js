9ee810a4599babdbbaba6e11e2f0fd4dbc28f339
Gohil Gautam <gohil.g.m9764@gmail.com>
2026-04-26 11:49:57 +0530

    day-7/array/array_toString

---
+++ day-7/Array/array_toString.js
@@ -0,0 +1,9 @@
+
+
+console.log("array toString");
+
+let fruits = ["mango", "litchi", "guava", "mango", "papita"];
+
+let str = fruits.toString();
+console.log(fruits);
+console.log(str, typeof str);
