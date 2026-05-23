e1e33d8db0c540c62061eb10d80c022108035e7a
Gohil Gautam <gohil.g.m9764@gmail.com>
2026-05-23 21:43:09 +0530

    day-3 / arithmetic

---
+++ day-3/arithmetic.js
@@ -0,0 +1,12 @@
+console.log("Arithmetic Operations in JavaScript");
+
+let a = 10;
+let b = 5;      
+
+console.log("Values of a and b:", a , b);
+
+console.log("Addition: " + (a + b)); // Addition: 15
+console.log("Subtraction: " + (a - b)); // Subtraction: 5
+console.log("Multiplication: " + (a * b)); // Multiplication: 50
+console.log("Division: " + (a / b)); // Division: 2
+console.log("Modulus: " + (a % b)); // Modulus: 0
