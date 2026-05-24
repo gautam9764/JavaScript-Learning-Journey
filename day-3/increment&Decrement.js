f8996e5fcdd70a2f7d0761a2b477904019717e51
Gohil Gautam <gohil.g.m9764@gmail.com>
2026-05-24 22:10:44 +0530

    day-3 / increment&Decrement

---
+++ day-3/increment&Decrement.js
@@ -0,0 +1,36 @@
+
+console.log("increment and Decrement Operations in JavaScript");
+
+let num = 10;// Initial value
+
+console.log("Initial Value of num:", num ); // Outputs 10
+console.log("Post-increment (num++): " + (num++)); // Outputs 10, then num becomes 11
+console.log("Value of num after Post-increment:", num ); // Outputs 11
+
+num = 10; // Resetting num to 10
+
+console.log("Pre-increment (++num): " + (++num)); // num becomes 11, then outputs 11
+console.log("Value of num after Pre-increment:", num ); // Outputs 11
+
+num = 10; // Resetting num to 10
+
+console.log("Post-decrement (num--): " + (num--)); // Outputs 10, then num becomes 9
+console.log("Value of num after Post-decrement:", num ); // Outputs 9
+
+num = 10; // Resetting num to 10
+
+console.log("Pre-decrement (--num): " + (--num)); // num becomes 9, then outputs 9
+console.log("Value of num after Pre-decrement:", num ); // Outputs 9
+
+
+  let a = 5;
+  let b = 5;
+  let c = 5;
+  let d = 5;
+  console.log("a =", a, "& b =", b, "& c =", c, "& d =", d);
+  console.log("a++ =", a++);
+  console.log("a++ =", a++);
+  console.log("++b =", ++b);
+  console.log("c-- =", c--);
+  console.log("c-- =", c--);
+  console.log("--d =", --d);
