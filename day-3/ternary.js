d7ce0d2c853b12a0c06e5272906288397dd20958
Gohil Gautam <gohil.g.m9764@gmail.com>
2026-05-25 18:33:05 +0530

    day-3 / ternary

---
+++ day-3/ternary.js
@@ -0,0 +1,13 @@
+
+console.log("ternary Operations in JavaScript");
+
+let a = 5;
+let b = 2;
+console.log("a =", a, "& b =", b);
+
+
+let max = (a > b) ? a : b;
+console.log("The maximum value between a and b is:", max);
+
+a < b ? console.log("a is greater than b") : console.log("a is not greater than b");
+a > b ? console.log("a is greater than b") : console.log("a is not greater than b");
