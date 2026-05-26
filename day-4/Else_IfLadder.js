5843106fcd5b51c77220cbd5766ba29f3004af2e
Gohil Gautam <gohil.g.m9764@gmail.com>
2026-05-26 20:58:15 +0530

    day-4 Else_IfLadder

---
+++ day-4/Else_IfLadder.js
@@ -0,0 +1,10 @@
+console.log("Else-If Ladder");
+let age = 18;
+
+if (age < 18) {
+    console.log("junior");
+} else if (age > 18) {
+    console.log("senior");
+} else {
+    console.log("middle");
+}
