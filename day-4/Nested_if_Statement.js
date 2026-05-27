3fa7b6217a1bec7866bd9c67d4baf20691f6e135
Gohil Gautam <gohil.g.m9764@gmail.com>
2026-05-27 21:34:23 +0530

    day-4 Nested_if_Statement

---
+++ day-4/Nested_if_Statement.js
@@ -0,0 +1,12 @@
+console.log("Nested if Statement");
+let a = 0;
+
+if (a > 0) {
+    if (a % 2 == 0) {
+        console.log("a is a positive even number");
+    } else {
+        console.log("a is a positive odd number");
+    }
+} else {
+    console.log("a is not a positive number");
+}
