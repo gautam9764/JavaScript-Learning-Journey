3a4d3565f79e1a5d8a6c851f33b7a1a068afb58c
Gohil Gautam <gohil.g.m9764@gmail.com>
2026-05-30 22:43:55 +0530

    day-5 forof_loop

---
+++ day-5/forof_loop.js
@@ -0,0 +1,16 @@
+
+console.log("for of loop");
+
+// Array example (values)
+const fruits = ["apple", "banana", "mango"];
+
+for (let fruit of fruits) {
+    console.log(fruit);
+}
+
+// String example
+const names = "JS";
+
+for (let ch of names) {
+    console.log(ch);
+}
