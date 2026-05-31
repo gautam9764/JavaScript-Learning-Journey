cf5ec2abe04aba739ea4d434ea5623c6804f919c
Gohil Gautam <gohil.g.m9764@gmail.com>
2026-05-31 19:36:37 +0530

    day-7 arrow_function

---
+++ day-7/arrow_function.js
@@ -0,0 +1,13 @@
+// arrow function
+console.log("arrow function");
+
+let alpha = () => {
+    console.log("hello"); // output: hello
+};
+
+alpha();
+
+let alpha2 = () => {
+    console.log("hello manas"); // output: hello manas
+};
+alpha2();
